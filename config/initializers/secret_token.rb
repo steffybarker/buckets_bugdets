9c2f1c4d858ca56b0c0d15bfbce0abca5748e9de
steffybarker <stefany.wfbarker@gmail.com>
2013-08-20 18:43:36 +0100

    Initial import scaffold bucket

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BucketsBudgets::Application.config.secret_key_base = '85f45db36b6963dd0d53d119831aa4d171ad9edddd6f00c1ed3a6593f6c0d3cbe9a080e9a4e68b08eba6f3f3ce1d06d5cb7d6b41fef329548a2dcffce77de61d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BucketsBudgets::Application.config.session_store :cookie_store, key: '_buckets_budgets_session'
